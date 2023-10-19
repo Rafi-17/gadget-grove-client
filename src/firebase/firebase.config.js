51bfbc164c392e0c7362bf03c46d582424d68f46
Rafi <tahmidulrafi13@gmail.com>
2023-10-19 19:04:03 +0600

    Login registration navbar banner added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzfUFP7Egh5__e4Pjyn6ychJLu5bkuQS0",
+  authDomain: "gadget-grove-7e7f4.firebaseapp.com",
+  projectId: "gadget-grove-7e7f4",
+  storageBucket: "gadget-grove-7e7f4.appspot.com",
+  messagingSenderId: "344453437286",
+  appId: "1:344453437286:web:ef68289e92638e17fa1aa7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
