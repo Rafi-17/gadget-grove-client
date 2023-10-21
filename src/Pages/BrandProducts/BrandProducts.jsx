5dadc14934c3e32311c963ac1d410c905dbe9e3c
Rafi <tahmidulrafi13@gmail.com>
2023-10-21 23:28:14 +0600

    Brand product loading issue solve and page reload from the top

--- src/Pages/BrandProducts/BrandProducts.jsx
+++ src/Pages/BrandProducts/BrandProducts.jsx
@@ -1,7 +1,13 @@
 import { useLoaderData, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import UniqueBrandCard from "../../Components/UniqueBrandCard/UniqueBrandCard";
+import { AuthContext } from "../../Providers/AuthProvider";
 const BrandProducts = () => {
+    useEffect(() => {
+        // Scroll to the top of the page when the component mounts
+        window.scrollTo(0, 0);
+      }, []);
+    const{loading} =useContext(AuthContext);
     const products=useLoaderData();
 
     const {brand}=useParams();
@@ -27,26 +33,30 @@ const BrandProducts = () => {
         setimg3(selectedBrand?.slide3)
         console.log(img1);
     },[allBrand, brand, img1])
+    if(loading){
+        return <span className="loading loading-spinner loading-lg"></span>
+    }
 
     return (
+        
         <div className="px-4 md:px-10 mt-10 max-w-[1440px] mx-auto">
             <div className="carousel w-full mb-10" >
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src={img1} className="w-full h-[80vh]" />
+                    <img src={img1} className="w-full h-[50vh] md:h-[80vh] " />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide4" className="btn btn-circle">❮</a> 
                     <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div> 
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src={img2} className="w-full h-[80vh]" />
+                    <img src={img2} className="w-full h-[50vh] md:h-[80vh] " />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a> 
                     <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div> 
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src={img3} className="w-full h-[80vh]" />
+                    <img src={img3} className="w-full h-[50vh] md:h-[80vh] " />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a> 
                     <a href="#slide1" className="btn btn-circle">❯</a>
@@ -61,7 +71,7 @@ const BrandProducts = () => {
                     <h2 className="text-center text-5xl font-bold text-orange-500 mb-10">Total Products: {products.length}</h2>
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
                         {
-                            products.map(product=><UniqueBrandCard key={product._id} product={product}></UniqueBrandCard>)
+                            products?.map(product=><UniqueBrandCard key={product._id} product={product}></UniqueBrandCard>)
                         }
                     </div> 
                 </div>
